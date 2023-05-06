a8647226665205602bacda9f47e4c8c6dd4e638c
Zinash <zinu32@gmail.com>
2023-05-06 01:42:05 -0500

    first commit

---
+++ api/users/user.router.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const auth = require('../middleware/auth');
+const { createUser, getUsers, getUserById, login } = require('./user.controller');
+
+router.post("/", createUser);
+router.get("/all", getUsers);
+router.get("/", auth, getUserById);
+router.post("/login",login);
+
+module.exports = router;
+
